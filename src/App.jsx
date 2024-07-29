b145ba25d134c1b5664da4b339e2edb1a2e78a42
victorr <kolawoledamilola06@gmail.com>
2024-07-29 00:25:35 -0400

    Work section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 import Skills from "./components/Skills"
+import Work from "./components/Work"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   )
 }
